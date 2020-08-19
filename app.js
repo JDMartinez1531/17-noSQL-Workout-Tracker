05303612b606cfb374ce8d7c0fb9eb124b28e12d
JDMartinez1531 <63159799+JDMartinez1531@users.noreply.github.com>
2020-08-19 15:56:19 -0500

    added get post and put routes

--- app.js
+++ app.js
@@ -8,9 +8,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-const PORT = process.env.PORT || 3000;
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+const PORT = process.env.PORT || 3000;
+
+
 
 // Add routes
 app.use(routes);
@@ -18,6 +20,15 @@ app.use(routes);
 app.get("/", function (req, res) {
 	res.sendFile(path.join(__dirname, "./public/index.html"));
 });
+
+app.get("/exercise", function (req, res) {
+	res.sendFile(path.join(__dirname, "./public/exercise.html"));
+});
+
+app.get("/stats", function (req, res) {
+	res.sendFile(path.join(__dirname, "./public/stats.html"));
+});
+
 
 app.listen(PORT, function () {
 	console.log("App listening on PORT " + PORT);
