549d9f3fcf12fede5c8c9f59be61dea742366149
JDMartinez1531 <63159799+JDMartinez1531@users.noreply.github.com>
2020-08-19 15:18:09 -0500

    added routes to app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const mongoose = require("mongoose");
+const routes = require("./controllers");
 // connect to Json and Connect PORT and connected Public folder to app
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -11,9 +12,11 @@ const PORT = process.env.PORT || 3000;
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
-app.get("/api/workouts", function (req, res) {
-    res.sendFile(path.join(__dirname, "index.html"));
-    return res;
+// Add routes
+app.use(routes);
+
+app.get("/", function (req, res) {
+	res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
 app.listen(PORT, function () {
