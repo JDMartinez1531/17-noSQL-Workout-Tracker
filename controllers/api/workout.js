987fec761a3349564e31f32ac94a645750027ded
JDMartinez1531 <63159799+JDMartinez1531@users.noreply.github.com>
2020-08-20 17:22:37 -0500

    added function to get data from a range of dates

--- controllers/api/workout.js
+++ controllers/api/workout.js
@@ -9,12 +9,14 @@ router.get("/", (req, res) => {
 		.catch((err) => res.json(err));
 });
 
+// add workout
 router.post("/", (req, res) => {
 	db.Workout.create(req.body)
 		.then((workout) => res.json(workout))
 		.catch((err) => res.json(err));
 });
 
+// update data with id
 router.put("/:id", (req, res) => {
 	db.Workout.findOneAndUpdate(
 		{ _id: mongoose.Types.ObjectId(req.params.id) },
@@ -22,6 +24,23 @@ router.put("/:id", (req, res) => {
 		{ new: true }
 	)
 		.then((workout) => res.json(workout))
+		.catch((err) => res.json(err));
+});
+
+const weekRange = () => {
+	let week = new Date(Date.now() - 6 * 24 * 60 * 60 * 1000);
+	// at midnight
+	week.setHours(0, 0, 0, 0);
+	// returning the days
+	return week;
+};
+
+// workouts in the last 7 days
+router.get("/range", (req, res) => {
+	db.Workout.find({
+		day: { $gte: weekRange() },
+	})
+		.then((workouts) => res.json(workouts))
 		.catch((err) => res.json(err));
 });
 
